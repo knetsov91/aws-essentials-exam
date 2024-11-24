99acb9b4533dcb2addd8fa659128bb5b733a7915
Konstantin <knetsov91@gmail.com>
2024-11-24 10:01:46 +0200

    Create AWS Lambda for image upload handling with CDK.

--- lib/regular_exam-stack.ts
+++ lib/regular_exam-stack.ts
@@ -1,4 +1,8 @@
 import * as cdk from 'aws-cdk-lib';
+import { LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { Lambda } from 'aws-cdk-lib/aws-ses-actions';
 import { Construct } from 'constructs';
  
 
@@ -6,6 +10,14 @@ export class RegularExamStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-
+    const imageUploadFunction = new NodejsFunction(
+      this,
+      "imageUploadFunction",
+      {
+        runtime: Runtime.NODEJS_20_X,
+        handler: "handler",
+        entry: `${__dirname}/../src/imageUploadFunction.ts`
+    });
+ 
   }
 }
