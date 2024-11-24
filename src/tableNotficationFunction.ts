dd7930faa8e88546da7396a7db0c6b688efd6977
Konstantin <knetsov91@gmail.com>
2024-11-24 14:41:18 +0200

    Add function to send sns notifications

--- src/tableNotficationFunction.ts
+++ src/tableNotficationFunction.ts
@@ -1,10 +1,18 @@
 import { PublishCommand, SNSClient } from '@aws-sdk/client-sns';
 export const handler = async (event: any) => {
     const snsClient = new SNSClient()
+    const topicArn = process.env.TOPIC_ARN;
+
     console.log(event);
+
+    await snsClient.send(new PublishCommand({
+        TopicArn: topicArn,
+        Message: `validJSON ${event.text}`
+    }))
+    console.log("Notification send");
     return {
         statusCode: 200,
          
-        body: "da"
+        body: "Notification send"
     }
 }
